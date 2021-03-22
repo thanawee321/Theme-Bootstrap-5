73559b7861a7d0445abad479c4f4840139a13e9d
thanawee321 <53590032+thanawee321@users.noreply.github.com>
2021-03-22 07:49:10 +0700

    Add files via upload

--- asset/sheet.js
+++ asset/sheet.js
@@ -9,3 +9,15 @@ $(document).ready(function(){
 
 
 });
+
+function showpass() {
+
+    var pass = document.getElementById('password');
+
+    if (pass.type == 'password') {
+
+        pass.type = 'text';
+    } else {
+        pass.type = 'password';
+    }
+}
